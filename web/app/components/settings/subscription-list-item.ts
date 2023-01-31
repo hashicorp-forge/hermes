e50e0af7a35c69d2744d0c23312054dd6b11e00c
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-01-31 16:04:06 -0500

    Add micro notifications for subscribing/removing

--- web/app/components/settings/subscription-list-item.ts
+++ web/app/components/settings/subscription-list-item.ts
@@ -3,6 +3,9 @@ import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import AuthenticatedUserService from "hermes/services/authenticated-user";
 import { assert } from "@ember/debug";
+import { tracked } from "@glimmer/tracking";
+import { restartableTask, task, timeout } from "ember-concurrency";
+import Ember from "ember";
 
 interface SettingsSubscriptionListItemComponentSignature {
   Args: {
@@ -10,8 +13,13 @@ interface SettingsSubscriptionListItemComponentSignature {
   };
 }
 
+const POPOVER_SHOW_DURATION = Ember.testing ? 0 : 1500;
+
 export default class SettingsSubscriptionListItemComponent extends Component<SettingsSubscriptionListItemComponentSignature> {
   @service declare authenticatedUser: AuthenticatedUserService;
+
+  @tracked popoverIsShown = false;
+  @tracked popoverIsClosing = false;
 
   /**
    * Determines whether the user is subscribed to the product area.
@@ -32,9 +40,25 @@ export default class SettingsSubscriptionListItemComponent extends Component<Set
    */
   @action protected toggleChecked(): void {
     if (this.isChecked) {
-      this.authenticatedUser.removeSubscription.perform(this.args.productArea);
+      void this.authenticatedUser.removeSubscription.perform(
+        this.args.productArea
+      );
     } else {
-      this.authenticatedUser.addSubscription.perform(this.args.productArea);
+      void this.authenticatedUser.addSubscription.perform(
+        this.args.productArea
+      );
     }
+    this.showPopover.perform();
   }
+
+  showPopover = restartableTask(async () => {
+    // this.popoverIsShown = false;
+    this.popoverIsClosing = false;
+    this.popoverIsShown = true;
+    await timeout(POPOVER_SHOW_DURATION * .75);
+    this.popoverIsClosing = true;
+    await timeout(POPOVER_SHOW_DURATION * .25);
+    this.popoverIsShown = false;
+    this.popoverIsClosing = false;
+  });
 }
