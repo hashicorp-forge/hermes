3eb2e0a6b3c6c9793794ee5de2bd4a82467871e5
Matthew Irish <matthew@hashicorp.com>
2023-02-02 08:55:12 -0600

    bugfix: save target url in SessionStorage so it can be used after redirecting for login

--- web/app/routes/authenticated.ts
+++ web/app/routes/authenticated.ts
@@ -1,34 +1,24 @@
 import Route from "@ember/routing/route";
 import { inject as service } from "@ember/service";
-import ConfigService from "hermes/services/config";
-import AuthenticateController from "hermes/controllers/authenticate";
 import AuthenticatedUserService from "hermes/services/authenticated-user";
 import Transition from "@ember/routing/transition";
 
 export default class AuthenticatedRoute extends Route {
   @service declare session: any;
   @service declare authenticatedUser: AuthenticatedUserService;
-  @service("config") declare configSvc: ConfigService;
 
   async afterModel(): Promise<void> {
     // Load user info
     await this.authenticatedUser.loadInfo.perform();
   }
 
-  async beforeModel(transition: Transition): Promise<void> {
-    // Check if the request requires authentication and if so, preserve the URL
-    let requireAuthentication = this.session.requireAuthentication(
-      transition,
-      "authenticate"
-    );
-
-    if (!requireAuthentication && transition.to.name != "authenticated.index") {
-      let authenticateController = this.controllerFor(
-        "authenticate"
-      ) as AuthenticateController;
-
-      // Set previous transition to preserve URL
-      authenticateController.previousTransition = transition;
+  async beforeModel(transition: any): Promise<void> {
+    let target = sessionStorage.getItem(this.session.SESSION_STORAGE_KEY);
+    if (transition.to.name != "authenticated.index" && !target) {
+      // apparently transition.intent is private or at least not documented so typing
+      // fails on this line
+      // ember-simple-auth uses this value to set cookies when fastboot is enabled: https://github.com/mainmatter/ember-simple-auth/blob/a7e583cf4d04d6ebc96b198a8fa6dde7445abf0e/packages/ember-simple-auth/addon/-internals/routing.js#L12
+      sessionStorage.setItem(this.session.SESSION_STORAGE_KEY, transition.intent.url);
     }
   }
 }
