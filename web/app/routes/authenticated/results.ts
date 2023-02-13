6d85ddadbe095fbd48a74701a4e0bbe0603f4377
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-02-13 17:20:11 -0500

    Remove sortBy params from Results route

--- web/app/routes/authenticated/results.ts
+++ web/app/routes/authenticated/results.ts
@@ -27,14 +27,11 @@ export default class ResultsRoute extends Route {
     },
     q: {
       refreshModel: true,
-    }
+    },
   };
 
   async model(params: ResultsRouteParams) {
-    const searchIndex =
-      params.sortBy === "dateAsc"
-        ? this.configSvc.config.algolia_docs_index_name + "_createdTime_asc"
-        : this.configSvc.config.algolia_docs_index_name + "_createdTime_desc";
+    const searchIndex = this.configSvc.config.algolia_docs_index_name;
 
     return RSVP.hash({
       facets: this.algolia.getFacets.perform(searchIndex, params),
