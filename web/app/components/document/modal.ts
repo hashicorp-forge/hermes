f348adbd1498b249b8b565d85c3e9e389b6c338c
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-02-27 14:40:04 -0500

    Componentize Modal, update HDS package

---
+++ web/app/components/document/modal.ts
@@ -0,0 +1,38 @@
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { task } from "ember-concurrency";
+
+interface DocumentModalComponentSignature {
+  Args: {
+    errorIsShown: boolean;
+    errorTitle: string;
+    errorDescription: string;
+    taskButtonText: string;
+    taskButtonLoadingText: string;
+    taskButtonIcon?: string;
+    taskButtonIsDisabled: boolean;
+    dismissError: () => void;
+    close: () => void;
+    task: () => Promise<void>;
+  };
+  Blocks: {
+    default: [
+      {
+        taskIsRunning: boolean;
+      }
+    ];
+  };
+}
+
+export default class DocumentModalsArchiveComponent extends Component<DocumentModalComponentSignature> {
+  @tracked taskIsRunning = false;
+
+  protected task = task(async () => {
+    try {
+      this.taskIsRunning = true;
+      await this.args.task();
+    } catch {
+      this.taskIsRunning = false;
+    }
+  });
+}
