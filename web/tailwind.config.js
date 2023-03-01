2098d610894ddbf569f4cf1c3500b5d7866491af
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-03-01 17:47:09 -0500

    Start of Doc::Thumbnail

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -43,6 +43,13 @@ module.exports = {
       bold: "var(--token-typography-font-weight-bold)",
     },
 
+    boxShadow: {
+      "surface-low": "var(--token-surface-low-box-shadow)",
+      "surface-mid": "var(--token-surface-mid-box-shadow)",
+      "elevation-low": "var(--token-elevation-low-box-shadow)",
+      "elevation-mid": "var(--token-elevation-mid-box-shadow)",
+    },
+
     extend: {
       colors: {
         // Note: Because the tokens are HEX and not RGB values,
