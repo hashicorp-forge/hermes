7fa9ba75ec54a255f4f537317448b64fcedecdd4
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-03-03 11:37:57 -0500

    Design tweaks

---
+++ web/app/components/doc/folder-affordance.ts
@@ -0,0 +1,10 @@
+import Component from "@glimmer/component";
+
+interface DocFolderAffordanceSignature {
+  Element: null;
+  Args: {
+    isLarge?: boolean;
+  };
+}
+
+export default class DocFolderAffordance extends Component<DocFolderAffordanceSignature> {}
