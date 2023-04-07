a62560faf4b7e3b2a6bf3da199b7fafa2c0c40ed
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-04-07 10:47:07 -0400

    Add front-end check

--- web/app/routes/authenticated.ts
+++ web/app/routes/authenticated.ts
@@ -7,13 +7,25 @@ export default class AuthenticatedRoute extends Route {
   @service declare session: SessionService;
   @service declare authenticatedUser: AuthenticatedUserService;
 
-  async beforeModel() {
+  async beforeModel(transition: any) {
     /**
+     * Checks if the session is authenticated in the front end.
+     * If unauthenticated, it will redirect to the auth screen
+     */
+    this.session.requireAuthentication(transition, "authenticate");
+
+    /**
+     * Checks if the session is authenticated in the back end.
      * If the `loadInfo` task returns a 401, it will bubble up to the
      * application error method which invalidates the session
      * and redirects to the auth screen.
      */
     await this.authenticatedUser.loadInfo.perform();
+
+    /**
+     * If the session is authenticated with the front- and back-ends,
+     * kick off the task to poll for expired auth.
+     */
     void this.session.pollForExpiredAuth.perform();
   }
 }
