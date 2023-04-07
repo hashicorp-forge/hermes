a303a4471fdb4e2a747a9431e0c466cee9897224
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-04-07 10:39:32 -0400

    Move storage call to Application route (#127)
    * Simplify session/localStorage logic; add isLoggedIn check
    
    * Revert application.js change
    
    * Add comment
    
    * Reduce diff
    
    * Move redirect storage call into Application route

--- web/app/routes/authenticated.ts
+++ web/app/routes/authenticated.ts
@@ -1,14 +1,13 @@
 import Route from "@ember/routing/route";
 import { inject as service } from "@ember/service";
 import AuthenticatedUserService from "hermes/services/authenticated-user";
-import window from "ember-window-mock";
-import SessionService, { REDIRECT_STORAGE_KEY } from "hermes/services/session";
+import SessionService from "hermes/services/session";
 
 export default class AuthenticatedRoute extends Route {
   @service declare session: SessionService;
   @service declare authenticatedUser: AuthenticatedUserService;
 
-  async afterModel(transition: any): Promise<void> {
+  async beforeModel(transition: any): Promise<void> {
     // If the user isn't authenticated, transition to the auth screen
     let isLoggedIn = this.session.requireAuthentication(
       transition,
@@ -18,30 +17,5 @@ export default class AuthenticatedRoute extends Route {
       await this.authenticatedUser.loadInfo.perform();
       void this.session.pollForExpiredAuth.perform();
     }
-  }
-
-  beforeModel(transition: any): void {
-    /**
-     * We expect a `transition.intent.url`, but in rare cases, it's undefined,
-     * e.g., when clicking the "view dashboard" button from the 404 route.
-     * When this happens, we fall back to `transition.to.name`.
-     *
-     * For reference:
-     * `transition.intent.url` e.g., 'documents/1'
-     * `transition.to.name` e.g., 'authenticated.documents'
-     */
-    let transitionTo = transition.intent.url ?? transition.to.name;
-
-    /**
-     * Capture the transition intent and save it to session/localStorage.
-     */
-    window.sessionStorage.setItem(REDIRECT_STORAGE_KEY, transitionTo);
-    window.localStorage.setItem(
-      REDIRECT_STORAGE_KEY,
-      JSON.stringify({
-        url: transitionTo,
-        expiresOn: Date.now() + 60 * 5000, // 5 minutes
-      })
-    );
   }
 }
