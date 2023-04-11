d4736e68463166ec1d47f6b93e6a3cd1e9b77f33
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-04-11 17:11:52 -0400

    Revert change; Add notes

--- web/app/authenticators/torii.ts
+++ web/app/authenticators/torii.ts
@@ -15,11 +15,17 @@ export default class ToriiAuthenticator extends Torii {
      * A rejecting promise indicates invalid session data and will result
      * in the session being invalidated or remaining unauthenticated.
      */
-    return this.fetchSvc.fetch("/api/v1/me", {
+    await this.fetchSvc.fetch("/api/v1/me", {
       method: "HEAD",
       headers: {
         "Hermes-Google-Access-Token": data.access_token,
       },
     });
+
+    /**
+     * If the fetch request above resolves, the session is valid and
+     * we can return the `restore` promise.
+     */
+    return super.restore(...arguments);
   }
 }
