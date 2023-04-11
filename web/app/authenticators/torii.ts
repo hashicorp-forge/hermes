08c2997794761f6cd5f73ae246c7ce41a8a7717c
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-04-11 18:11:57 -0400

    Revise torii promise

--- web/app/authenticators/torii.ts
+++ web/app/authenticators/torii.ts
@@ -15,17 +15,13 @@ export default class ToriiAuthenticator extends Torii {
      * A rejecting promise indicates invalid session data and will result
      * in the session being invalidated or remaining unauthenticated.
      */
-    await this.fetchSvc.fetch("/api/v1/me", {
-      method: "HEAD",
-      headers: {
-        "Hermes-Google-Access-Token": data.access_token,
-      },
-    });
-
-    /**
-     * If the fetch request above resolves, the session is valid and
-     * we can return the `restore` promise.
-     */
-    return super.restore(...arguments);
+    return this.fetchSvc
+      .fetch("/api/v1/me", {
+        method: "HEAD",
+        headers: {
+          "Hermes-Google-Access-Token": data.access_token,
+        },
+      })
+      .then(() => data);
   }
 }
