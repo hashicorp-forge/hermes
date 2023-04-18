700bc661f6ac15c02d31ad69de4e0770c236b3c6
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-04-18 16:48:27 -0400

    Reconfigure Ember Metrics / Google Analytics (#143)
    * Get analytics config from the service
    
    * Cleanup
    
    * Remove unnecessary import
    
    * Update google-analytics-four.ts
    
    * Fix line reference
    
    * Enable config via ENV/Service
    
    * Update web/app/metrics-adapters/google-analytics-four.ts
    
    Co-authored-by: Josh Freda <jfreda@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Josh Freda <jfreda@users.noreply.github.com>

--- web/types/ember-metrics/services/metrics.d.ts
+++ web/types/ember-metrics/services/metrics.d.ts
@@ -2,9 +2,13 @@
 
 import Service from "@ember/service";
 
-declare module "ember-metrics/index" {
+declare module "ember-metrics/services/metrics" {
   // Note: Incomplete types; only the methods we use are defined.
   export default class EmberMetricsService extends Service {
+    _adapters: any;
+    _options: any;
+    _activateAdapter({}): unknown;
+    _lookupAdapter(adapterName: string): any;
     trackPage(): void;
   }
 }
