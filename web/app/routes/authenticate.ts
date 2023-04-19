8d11b85038a6155bb28efc9c6ea4c6853272a247
Josh Freda <jfreda@users.noreply.github.com>
2023-04-19 16:59:19 -0500

    Redirect right away from the authenticate route if skipping Google auth

--- web/app/routes/authenticate.ts
+++ web/app/routes/authenticate.ts
@@ -1,19 +1,27 @@
 import Route from "@ember/routing/route";
 import { inject as service } from "@ember/service";
 import ConfigService from "hermes/services/config";
+import RouterService from "@ember/routing/router-service";
 import SessionService from "hermes/services/session";
 
 export default class AuthenticateRoute extends Route {
   @service("config") declare configSvc: ConfigService;
+  @service declare router: RouterService;
   @service declare session: SessionService;
 
   beforeModel() {
     /**
-     * If using Google auth, checks if the session is authenticated,
+     * If we're skipping Google auth, redirect right away because this route
+     * isn't useful.
+     */
+    if (this.configSvc.config.skip_google_auth) {
+      this.router.replaceWith("/");
+    }
+
+    /**
+     * Checks if the session is authenticated,
      * and if it is, transitions to the specified route
      */
-    if (!this.configSvc.config.skip_google_auth) {
-      this.session.prohibitAuthentication("/");
-    }
+    this.session.prohibitAuthentication("/");
   }
 }
