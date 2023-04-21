91c9d884817e501acbac03b7199b18f7fb1871a0
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-04-21 16:11:30 -0400

    Add comments to index

--- web/app/components/x/dropdown-list/index.ts
+++ web/app/components/x/dropdown-list/index.ts
@@ -29,27 +29,35 @@ export default class XDropdownListComponent extends Component<
   @service("fetch") declare fetchSvc: FetchService;
 
   @tracked private _scrollContainer: HTMLElement | null = null;
+  @tracked private _input: HTMLInputElement | null = null;
+  @tracked private _filteredItems: unknown | null = null;
+  @tracked private _menuItems: NodeListOf<Element> | null = null;
+
   @tracked protected query: string = "";
-
   @tracked protected listItemRole = this.inputIsShown ? "option" : "menuitem";
-
   @tracked protected focusedItemIndex = -1;
 
-  @tracked filteredItems: unknown | null = null;
-  @tracked protected menuItems: NodeListOf<Element> | null = null;
-
-  @tracked _input: HTMLInputElement | null = null;
-
+  /**
+   * An asserted-true reference to the scroll container.
+   */
   private get scrollContainer(): HTMLElement {
     assert("_scrollContainer must exist", this._scrollContainer);
     return this._scrollContainer;
   }
 
+  /**
+   * An asserted-true reference to the filter input.
+   */
   get input() {
     assert("input must exist", this._input);
     return this._input;
   }
 
+  /**
+   * Whether the dropdown has a filter input.
+   * Used to determine layout configurations and
+   * aria-roles for various elements.
+   */
   get inputIsShown() {
     if (!this.args.items) {
       return false;
@@ -58,10 +66,18 @@ export default class XDropdownListComponent extends Component<
     }
   }
 
+  /**
+   * The items that should be shown in the dropdown.
+   * Initially the same as the items passed in and
+   * updated when the user types in the filter input.
+   */
   get shownItems() {
-    return this.filteredItems || this.args.items;
+    return this._filteredItems || this.args.items;
   }
 
+  /**
+   * The "aria-controls" value for the dropdown trigger.
+   */
   get ariaControls() {
     let value = "x-dropdown-";
     if (this.inputIsShown) {
@@ -72,30 +88,56 @@ export default class XDropdownListComponent extends Component<
     return `${value}-`;
   }
 
+  /**
+   * The action run when the scrollContainer is inserted.
+   * Registers the div for reference locally.
+   */
   @action protected registerScrollContainer(element: HTMLDivElement) {
     this._scrollContainer = element;
   }
 
+  /**
+   * The action performed when the filter input is inserted.
+   * Registers the input locally and focuses it for the user.
+   */
   @action registerAndFocusInput(e: HTMLInputElement) {
     this._input = e;
     this.input.focus();
   }
 
-  @action protected didInsertList() {
+  /**
+   * The action run when the content div is inserted.
+   * Used to assign ids to the menu items.
+   */
+  @action protected didInsertContent() {
     schedule("afterRender", () => {
-      assert("didInsertList expects a _scrollContainer", this._scrollContainer);
+      assert(
+        "didInsertContent expects a _scrollContainer",
+        this._scrollContainer
+      );
       this.assignMenuItemIDs(
         this._scrollContainer.querySelectorAll(`[role=${this.listItemRole}]`)
       );
     });
   }
 
-  @action willDestroyDropdown() {
-    this.filteredItems = null;
+  /**
+   * The action run when the content div is destroyed.
+   * Resets the filtered items so that the next time the
+   * popover is opened, the full list of items is shown.
+   */
+  @action resetFilteredItems() {
+    this._filteredItems = null;
   }
 
+  /**
+   * The action run when the popover is inserted, and when
+   * the user filters or navigates the dropdown.
+   * Loops through the menu items and assigns an id that
+   * matches the index of the item in the list.
+   */
   @action assignMenuItemIDs(items: NodeListOf<Element>): void {
-    this.menuItems = items;
+    this._menuItems = items;
     for (let i = 0; i < items.length; i++) {
       let item = items[i];
       assert("item must exist", item instanceof HTMLElement);
@@ -103,11 +145,48 @@ export default class XDropdownListComponent extends Component<
     }
   }
 
+  /**
+   * The action run when the trigger is focused and the user
+   * presses the up or down arrow keys. Used to open and focus
+   * to the first or last item in the dropdown.
+   */
+  @action protected onTriggerKeydown(
+    contentIsShown: boolean,
+    showContent: () => void,
+    event: KeyboardEvent
+  ) {
+    if (contentIsShown) {
+      return;
+    }
+
+    if (event.key === "ArrowUp" || event.key === "ArrowDown") {
+      event.preventDefault();
+      showContent();
+
+      // Wait for the menuItems to be set by the showContent action.
+      next(() => {
+        switch (event.key) {
+          case "ArrowDown":
+            this.setFocusedItemIndex(FocusDirection.First, false);
+            break;
+          case "ArrowUp":
+            this.setFocusedItemIndex(FocusDirection.Last);
+            break;
+        }
+      });
+    }
+  }
+
+  /**
+   * Sets the focus to the next or previous menu item.
+   * Used by the onKeydown action to navigate the dropdown, and
+   * by the FacetDropdownListItem component on mouseenter.s
+   */
   @action protected setFocusedItemIndex(
     focusDirectionOrNumber: FocusDirection | number,
     maybeScrollIntoView = true
   ) {
-    let { menuItems, focusedItemIndex } = this;
+    let { _menuItems: menuItems, focusedItemIndex } = this;
 
     let setFirst = () => {
       focusedItemIndex = 0;
@@ -161,12 +240,13 @@ export default class XDropdownListComponent extends Component<
     }
   }
 
-  @action protected resetFocusedItemIndex() {
-    this.focusedItemIndex = -1;
-  }
-
+  /**
+   * Checks whether the focused item is completely visible,
+   * and, if necessary, scrolls the dropdown to make it visible.
+   * Used by the setFocusedItemIndex action on keydown.
+   */
   private maybeScrollIntoView() {
-    const focusedItem = this.menuItems?.item(this.focusedItemIndex);
+    const focusedItem = this._menuItems?.item(this.focusedItemIndex);
     assert("focusedItem must exist", focusedItem instanceof HTMLElement);
 
     const containerTopPadding = 12;
@@ -184,49 +264,33 @@ export default class XDropdownListComponent extends Component<
     }
   }
 
-  @action protected onTriggerKeydown(
-    contentIsShown: boolean,
-    showContent: () => void,
-    event: KeyboardEvent
-  ) {
-    if (contentIsShown) {
-      return;
-    }
-
-    if (event.key === "ArrowUp" || event.key === "ArrowDown") {
-      // Stop the event from bubbling to the popover's keydown handler.
-      event.preventDefault();
-
-      showContent();
-
-      // Wait for the menuItems to be set by the showDropdown action.
-      next(() => {
-        switch (event.key) {
-          case "ArrowDown":
-            this.setFocusedItemIndex(FocusDirection.First, false);
-            break;
-          case "ArrowUp":
-            this.setFocusedItemIndex(FocusDirection.Last);
-            break;
-        }
-      });
-    }
+  /**
+   * Resets the focus index to its initial value.
+   * Called when the dropdown is closed, and when the input is focused.
+   */
+  @action protected resetFocusedItemIndex() {
+    this.focusedItemIndex = -1;
   }
 
+  /**
+   * The action run when the user types in the input.
+   * Filters the facets shown in the dropdown and schedules
+   * the menu items to be assigned their new IDs.
+   */
   protected onInput = restartableTask(async (inputEvent: InputEvent) => {
     this.focusedItemIndex = -1;
 
-    let showItems: any = {};
+    let shownItems: any = {};
     let { items } = this.args;
 
     this.query = (inputEvent.target as HTMLInputElement).value;
     for (const [key, value] of Object.entries(items)) {
       if (key.toLowerCase().includes(this.query.toLowerCase())) {
-        showItems[key] = value;
+        shownItems[key] = value;
       }
     }
 
-    this.filteredItems = showItems;
+    this._filteredItems = shownItems;
 
     schedule("afterRender", () => {
       assert("onInput expects a _scrollContainer", this._scrollContainer);
