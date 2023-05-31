ea2dbc1797b7ed0bd5cc8709cc96f6c00f631007
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-05-30 21:37:17 -0400

    Add BadgeDropdownList component (#185)
    * BadgeDropdownList component
    
    * BadgeDropdownList component
    
    * Add custom-item test
    
    * Remove unnecessary comment
    
    * Resolve merge conflict
    
    * Remove product specificity

---
+++ web/app/components/inputs/badge-dropdown-list.ts
@@ -0,0 +1,27 @@
+import { Placement } from "@floating-ui/dom";
+import Component from "@glimmer/component";
+
+interface InputsBadgeDropdownListComponentSignature {
+  Element: HTMLDivElement;
+  Args: {
+    items: any;
+    selected?: any;
+    listIsOrdered?: boolean;
+    isSaving?: boolean;
+    onItemClick: ((e: Event) => void) | ((e: string) => void);
+    placement?: Placement;
+    icon: string;
+  };
+  Blocks: {
+    default: [];
+    item: [dd: any];
+  };
+}
+
+export default class InputsBadgeDropdownListComponent extends Component<InputsBadgeDropdownListComponentSignature> {}
+
+declare module "@glint/environment-ember-loose/registry" {
+  export default interface Registry {
+    "Inputs::BadgeDropdownList": typeof InputsBadgeDropdownListComponent;
+  }
+}
