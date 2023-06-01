457546d0923aa3b524a93d08df391d7479b0e0a7
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-05-31 20:06:20 -0400

    Fix approver checkmarks

--- web/app/helpers/has-approved-doc.ts
+++ web/app/helpers/has-approved-doc.ts
@@ -1,10 +1,27 @@
 import { helper } from "@ember/component/helper";
 import { HermesDocument } from "hermes/types/document";
 
-export default helper(([document, approverEmail]: [HermesDocument, string]) => {
-  if (document.approvedBy) {
-    return document.approvedBy.some((email) => email === approverEmail);
-  } else {
-    return false;
+export interface HasApprovedDocSignature {
+  Args: {
+    Positional: [HermesDocument, string];
+  };
+  Return: boolean;
+}
+
+const hasApprovedDoc = helper<HasApprovedDocSignature>(
+  ([document, approverEmail]: [HermesDocument, string]) => {
+    if (document.approvedBy) {
+      return document.approvedBy.some((email) => email === approverEmail);
+    } else {
+      return false;
+    }
   }
-});
+);
+
+export default hasApprovedDoc;
+
+declare module "@glint/environment-ember-loose/registry" {
+  export default interface Registry {
+    "has-approved-doc": typeof hasApprovedDoc;
+  }
+}
