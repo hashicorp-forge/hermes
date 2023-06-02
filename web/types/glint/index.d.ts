0bb49f3cabda8127e949f827410897dfcd4a1425
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-06-02 17:29:39 -0400

    Fix Glint imports (#196)

--- web/types/glint/index.d.ts
+++ web/types/glint/index.d.ts
@@ -1,17 +1,28 @@
 import "@glint/environment-ember-loose";
+import PerformHelper from "ember-concurrency/helpers/perform";
+import OnDocumentHelper from "ember-on-helper/helpers/on-document";
+import DidInsertModifier from "ember-render-modifiers/modifiers/did-insert";
+import WillDestroyModifier from "ember-render-modifiers/modifiers/will-destroy";
+import AndHelper from "ember-truth-helpers/helpers/and";
+import EqHelper from "ember-truth-helpers/helpers/eq";
+import IsEmptyHelper from "ember-truth-helpers/helpers/is-empty";
+import LtHelper from "ember-truth-helpers/helpers/lt";
+import NotHelper from "ember-truth-helpers/helpers/not";
+import OrHelper from "ember-truth-helpers/helpers/or";
 import { FlightIconComponent } from "hds/flight-icon";
 
 declare module "@glint/environment-ember-loose/registry" {
   export default interface Registry {
-    "did-insert": typeof import("@gavant/glint-template-types/types/ember-render-modifiers/did-insert").default;
-    "on-document": typeof import("@gavant/glint-template-types/types/ember-on-helper/on-document").default;
-    perform: typeof import("@gavant/glint-template-types/types/ember-concurrency/perform").default;
-    or: typeof import("@gavant/glint-template-types/types/ember-truth-helpers/or").default;
-    eq: typeof import("@gavant/glint-template-types/types/ember-truth-helpers/eq").default;
-    and: typeof import("@gavant/glint-template-types/types/ember-truth-helpers/and").default;
-    not: typeof import("@gavant/glint-template-types/types/ember-truth-helpers/not").default;
-    "is-empty": typeof import("@gavant/glint-template-types/types/ember-truth-helpers/is-empty").default;
-
+    "did-insert": typeof DidInsertModifier;
+    "will-destroy": typeof WillDestroyModifier;
+    "on-document": typeof OnDocumentHelper;
+    perform: typeof PerformHelper;
+    or: typeof OrHelper;
+    eq: typeof EqHelper;
+    and: typeof AndHelper;
+    not: typeof NotHelper;
+    lt: typeof LtHelper
+    "is-empty": IsEmptyHelper;
     FlightIcon: FlightIconComponent;
   }
 }
