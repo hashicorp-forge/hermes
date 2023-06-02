b4b0552f244407ecd02bc875d9040de5eb8338e6
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-06-02 13:48:06 -0400

    Add ProductSelect component (#187)
    * BadgeDropdownList component
    
    * BadgeDropdownList component
    
    * Add custom-item test
    
    * Remove unnecessary comment
    
    * Resolve merge conflict
    
    * ProductSelect component
    
    * Add list item tests
    
    * Tests, styles, cleanup
    
    * Add onChange test
    
    * Pass @icon into component
    
    * Remove unused property
    
    * Revert unintended change
    
    * Padding tweak; Prettier

---
+++ web/app/components/inputs/product-select/item.ts
@@ -0,0 +1,18 @@
+import Component from "@glimmer/component";
+
+interface InputsProductSelectItemComponentSignature {
+  Element: HTMLDivElement;
+  Args: {
+    product: string;
+    selected: boolean;
+    abbreviation?: boolean;
+  };
+}
+
+export default class InputsProductSelectItemComponent extends Component<InputsProductSelectItemComponentSignature> {}
+
+declare module "@glint/environment-ember-loose/registry" {
+  export default interface Registry {
+    "Inputs::ProductSelect::Item": typeof InputsProductSelectItemComponent;
+  }
+}
