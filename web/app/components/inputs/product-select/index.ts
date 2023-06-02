9a1b5ccff1feb488da92131966d314c3981cb8c3
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-06-02 13:48:06 -0400

    Add ProductSelect component (#187)
    * BadgeDropdownList component
    
    * BadgeDropdownList component
    
    * Add custom-item test
    
    * Remove unnecessary comment
    
    * Resolve merge conflict
    
    * ProductSelect component
    
    * Add list item tests
    
    * Tests, styles, cleanup
    
    * Add onChange test
    
    * Pass @icon into component
    
    * Remove unused property
    
    * Revert unintended change
    
    * Padding tweak; Prettier

---
+++ web/app/components/inputs/product-select/index.ts
@@ -0,0 +1,76 @@
+import { assert } from "@ember/debug";
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
+import { Placement } from "@floating-ui/dom";
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { task } from "ember-concurrency";
+import FetchService from "hermes/services/fetch";
+import getProductId from "hermes/utils/get-product-id";
+
+interface InputsProductSelectSignature {
+  Element: HTMLDivElement;
+  Args: {
+    selected?: string;
+    onChange: (value: string) => void;
+    formatIsBadge?: boolean;
+    placement?: Placement;
+    isSaving?: boolean;
+  };
+}
+
+type ProductAreas = {
+  [key: string]: ProductArea;
+};
+
+type ProductArea = {
+  abbreviation: string;
+  perDocDataType: unknown;
+};
+
+export default class InputsProductSelectComponent extends Component<InputsProductSelectSignature> {
+  @service("fetch") declare fetchSvc: FetchService;
+
+  @tracked selected = this.args.selected;
+
+  @tracked products: ProductAreas | undefined = undefined;
+
+  get icon(): string {
+    let icon = "folder";
+    if (this.selected && getProductId(this.selected)) {
+      icon = getProductId(this.selected) as string;
+    }
+    return icon;
+  }
+
+  get selectedProductAbbreviation(): string | undefined {
+    if (!this.selected) {
+      return undefined;
+    }
+    assert("products must be loaded", this.products);
+    return this.products[this.selected]?.abbreviation;
+  }
+
+  @action onChange(newValue: any) {
+    this.selected = newValue;
+    this.args.onChange(newValue);
+  }
+
+  protected fetchProducts = task(async () => {
+    try {
+      let products = await this.fetchSvc
+        .fetch("/api/v1/products")
+        .then((resp) => resp?.json());
+      this.products = products;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  });
+}
+
+declare module "@glint/environment-ember-loose/registry" {
+  export default interface Registry {
+    "Inputs::ProductSelect": typeof InputsProductSelectComponent;
+  }
+}
