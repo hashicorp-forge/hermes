b307225d65dc14464f71ba076437b0ff44686865
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-06-02 17:30:10 -0400

    Register template helpers (#192)
    * Register GetProductID helper
    
    * Register helpers with Glint
    
    * Syntax tweak

--- web/app/helpers/add.js
+++
@@ -1,7 +0,0 @@
-import { helper } from "@ember/component/helper";
-
-export default helper(function add([first, second]) {
-  const firstInt = parseInt(first);
-  const secondInt = parseInt(second);
-  return firstInt + secondInt;
-});
---
+++ web/app/helpers/get-product-id.ts
@@ -0,0 +1,23 @@
+import { helper } from "@ember/component/helper";
+import getProductId from "hermes/utils/get-product-id";
+
+export interface GetProductIDSignature {
+  Args: {
+    Positional: [string | undefined];
+  };
+}
+
+const getProductIDHelper = helper<GetProductIDSignature>(([productName]) => {
+  if (!productName) {
+    return null;
+  }
+  return getProductId(productName);
+});
+
+export default getProductIDHelper;
+
+declare module "@glint/environment-ember-loose/registry" {
+  export default interface Registry {
+    "get-product-id": typeof getProductIDHelper;
+  }
+}
