a6913a428eaebb06f85ae4eba657a2289ebe49fa
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-06-14 17:22:16 -0400

    Register ExternalLink component with Glint (#214)
    Register ExternalLink

--- web/app/components/external-link.ts
+++ web/app/components/external-link.ts
@@ -2,6 +2,15 @@ import Component from "@glimmer/component";
 
 interface ExternalLinkComponentSignature {
   Element: HTMLAnchorElement;
+  Blocks: {
+    default: [];
+  };
 }
 
 export default class ExternalLinkComponent extends Component<ExternalLinkComponentSignature> {}
+
+declare module "@glint/environment-ember-loose/registry" {
+  export default interface Registry {
+    ExternalLink: typeof ExternalLinkComponent;
+  }
+}
