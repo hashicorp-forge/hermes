eb814d064f2db05daebcb2f04f542440706957f6
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-06-23 18:29:42 -0400

    Revert previous changes; update environment type

--- web/app/controllers/application.ts
+++ web/app/controllers/application.ts
@@ -1,14 +1,10 @@
 import Controller from "@ember/controller";
-import { inject as service } from "@ember/service";
-import ConfigService from "hermes/services/config";
 import config from "hermes/config/environment";
 
 export default class ApplicationController extends Controller {
-  @service("config") declare configSvc: ConfigService;
-
   protected get animatedToolsAreShown() {
     if (config.environment === "development") {
-      return this.configSvc.config.show_ember_animated_tools;
+      return config.showEmberAnimatedTools;
     } else {
       return false;
     }
