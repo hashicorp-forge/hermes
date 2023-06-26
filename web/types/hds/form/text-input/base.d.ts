19e33a82f8eab63175a5c8af1765bd7f3bfb0e11
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-06-26 11:14:22 -0400

    Make `type` optional

--- web/types/hds/form/text-input/base.d.ts
+++ web/types/hds/form/text-input/base.d.ts
@@ -1,11 +1,12 @@
 // https://helios.hashicorp.design/components/form/text-input?tab=code#formtextinputbase-1
+
 import { ComponentLike } from "@glint/template";
 import { HdsFormTextInputArgs } from ".";
 
 export interface HdsFormTextInputBaseComponentSignature {
   Element: HTMLInputElement;
   Args: {
-    type: string;
+    type?: string;
     value: string | number | Date;
     isInvalid?: boolean;
     width?: string;
