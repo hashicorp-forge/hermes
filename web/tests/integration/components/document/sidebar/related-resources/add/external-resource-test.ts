167daea217e9c4fcfa9750f6c2ae802528d43315
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-06-28 14:45:48 -0400

    Add test for `external-resource`

---
+++ web/tests/integration/components/document/sidebar/related-resources/add/external-resource-test.ts
@@ -0,0 +1,49 @@
+import { module, test } from "qunit";
+import { setupRenderingTest } from "ember-qunit";
+import { TestContext, render } from "@ember/test-helpers";
+import { hbs } from "ember-cli-htmlbars";
+
+interface DocumentSidebarRelatedResourcesAddExternalResourceTestContext
+  extends TestContext {
+  onSubmit: () => void;
+  onInput: () => void;
+  urlIsLoading: boolean;
+}
+
+module(
+  "Integration | Component | document/sidebar/related-resources/add/external-resource",
+  function (hooks) {
+    setupRenderingTest(hooks);
+
+    test("it renders as expected", async function (this: DocumentSidebarRelatedResourcesAddExternalResourceTestContext, assert) {
+      this.set("onSubmit", () => {});
+      this.set("onInput", () => {});
+      this.set("urlIsLoading", true);
+
+      await render<DocumentSidebarRelatedResourcesAddExternalResourceTestContext>(hbs`
+      <Document::Sidebar::RelatedResources::Add::ExternalResource
+        @urlIsLoading={{this.urlIsLoading}}
+        @title="Test"
+        @url="https://example.com"
+        @defaultFaviconIsShown={{true}}
+        @faviconURL="https://example.com/favicon.ico"
+        @onSubmit={{this.onSubmit}}
+        @onInput={{this.onInput}}
+      />
+    `);
+
+      assert
+        .dom("[data-test-external-resource-url-is-loading]")
+        .hasText(
+          "Reading URL...",
+          'shows "Reading URL..." when the URL is loading'
+        );
+
+      this.set("urlIsLoading", false);
+
+      assert
+        .dom("[data-test-external-resource-form]")
+        .exists("shows the form when the URL is not loading");
+    });
+  }
+);
