f76a151e176d9cea44a148c6c45476bef0ed677d
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-07-11 12:23:24 -0400

    Add "new" label

--- web/app/components/document/sidebar/section-header.ts
+++ web/app/components/document/sidebar/section-header.ts
@@ -6,6 +6,7 @@ interface DocumentSidebarSectionHeaderComponentSignature {
   Element: HTMLDivElement;
   Args: {
     title: string;
+    badgeText?: string;
     buttonLabel?: string;
     buttonAction?: () => void;
     buttonIcon?: string;
