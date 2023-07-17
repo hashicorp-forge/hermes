8d4e149a0f2b0683a2ac287487cf6fb4a6bb953c
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-07-17 10:54:03 -0400

    Improve external link submitting

--- web/app/components/document/sidebar/related-resources/add/external-resource.ts
+++ web/app/components/document/sidebar/related-resources/add/external-resource.ts
@@ -1,38 +1,24 @@
-import { action } from "@ember/object";
 import Component from "@glimmer/component";
-import { tracked } from "@glimmer/tracking";
 
 interface DocumentSidebarRelatedResourcesAddExternalResourceSignature {
   Element: null;
   Args: {
     title: string;
     url: string;
-    onSubmit: () => void;
+    onSubmit: (e: Event) => void;
     onInput: (e: Event) => void;
     linkIsDuplicate?: boolean;
+    titleErrorIsShown?: boolean;
   };
 }
 
 export default class DocumentSidebarRelatedResourcesAddExternalResource extends Component<DocumentSidebarRelatedResourcesAddExternalResourceSignature> {
-  @tracked titleErrorIsShown = false;
-
-  get errorIsShown() {
-    return this.titleErrorIsShown || this.args.linkIsDuplicate;
-  }
   /**
-   * The action run when the "add" button is clicked.
-   * Calls the parent method unless the link is a duplicate.
+   * Whether an error message should be shown.
+   * True if the title is empty or the URL is a duplicate.
    */
-  @action onSubmit() {
-    // if the link doesn't have a title, don't submit
-    if (!this.args.title) {
-      this.titleErrorIsShown = true;
-      return;
-    }
-
-    if (!this.args.linkIsDuplicate) {
-      this.args.onSubmit();
-    }
+  get errorIsShown() {
+    return this.args.titleErrorIsShown || this.args.linkIsDuplicate;
   }
 }
 
