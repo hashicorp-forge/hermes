a4e1e1007a99ff43ae816f7e4a29bcfc6ef884de
Anubhav Mishra <anubhavmishra@me.com>
2023-08-16 18:38:01 -0700

    version and revision included in config response

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,3 +1,31 @@
 package version
 
+import (
+	"runtime/debug"
+)
+
 const Version = "0.2.0"
+
+// GetVersion returns
+// the version number
+func GetVersion() string {
+	return Version
+}
+
+// GetShortRevision returns a short
+// commit or checkout revision
+// from build information
+func GetShortRevision() string {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.revision" {
+				var shortRevision string
+				if len(setting.Value) > 7 {
+					shortRevision = setting.Value[:7]
+				}
+				return shortRevision
+			}
+		}
+	}
+	return ""
+}
