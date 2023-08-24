68e8d0cf7875d5d0859f81a9ee60c50cdc98ad78
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-08-24 15:19:34 -0400

    Switch from SortDropdown to ColumnButton

--- web/app/controllers/authenticated/my.ts
+++ web/app/controllers/authenticated/my.ts
@@ -1,4 +1,8 @@
 import Controller from "@ember/controller";
+import { SortByValue } from "hermes/components/header/toolbar";
+import { SortDirection } from "hermes/components/table/sortable-header";
+import AuthenticatedMyRoute from "hermes/routes/authenticated/my";
+import { ModelFrom } from "hermes/types/route-models";
 
 export default class AuthenticatedMyController extends Controller {
   queryParams = ["docType", "owners", "page", "product", "sortBy", "status"];
@@ -8,4 +12,15 @@ export default class AuthenticatedMyController extends Controller {
   product = [];
   sortBy = "dateDesc";
   status = [];
+
+  declare model: ModelFrom<AuthenticatedMyRoute>;
+
+  get sortDirection() {
+    switch (this.model.sortedBy) {
+      case SortByValue.DateAsc:
+        return SortDirection.Asc;
+      default:
+        return SortDirection.Desc;
+    }
+  }
 }
