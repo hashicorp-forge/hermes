2da4a48be17f1ae74cbdb0a19839498ca3bc3b0c
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-08-25 15:32:16 -0400

    Update filter button styles (#310)
    * Update filter button styles
    
    * Revert sortDropdown deletion

--- web/app/components/header/facet-dropdown.ts
+++ web/app/components/header/facet-dropdown.ts
@@ -3,12 +3,19 @@ import { FacetDropdownObjects } from "hermes/types/facets";
 import { inject as service } from "@ember/service";
 import RouterService from "@ember/routing/router-service";
 
+enum FacetDropdownPosition {
+  Left = "left",
+  Center = "center",
+  Right = "right",
+}
+
 interface HeaderFacetDropdownComponentSignature {
   Element: HTMLDivElement;
   Args: {
     label: string;
     facets: FacetDropdownObjects | null;
     disabled?: boolean;
+    position: `${FacetDropdownPosition}`;
   };
 }
 
@@ -17,6 +24,18 @@ export default class HeaderFacetDropdownComponent extends Component<HeaderFacetD
 
   protected get currentRouteName() {
     return this.router.currentRouteName;
+  }
+
+  protected get positionIsLeft() {
+    return this.args.position === FacetDropdownPosition.Left;
+  }
+
+  protected get positionIsCenter() {
+    return this.args.position === FacetDropdownPosition.Center;
+  }
+
+  protected get positionIsRight() {
+    return this.args.position === FacetDropdownPosition.Right;
   }
 }
 
