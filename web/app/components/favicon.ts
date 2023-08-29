86986c635d0660d44b05a83528f0b89326028408
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-08-29 14:49:34 -0400

    Add icon fallback

--- web/app/components/favicon.ts
+++ web/app/components/favicon.ts
@@ -1,13 +1,23 @@
+import { action } from "@ember/object";
 import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
 
 interface FaviconComponentSignature {
-  Element: HTMLImageElement;
+  Element: SVGElement | HTMLImageElement;
   Args: {
     url: string;
   };
 }
 
-export default class FaviconComponent extends Component<FaviconComponentSignature> {}
+export default class FaviconComponent extends Component<FaviconComponentSignature> {
+  @tracked protected fallbackIconIsShown = false;
+
+  protected readonly faviconURL = `https://www.google.com/s2/favicons?sz=64&domain=${this.args.url}`;
+
+  @action protected showFallbackIcon() {
+    this.fallbackIconIsShown = true;
+  }
+}
 
 declare module "@glint/environment-ember-loose/registry" {
   export default interface Registry {
--- web/tests/integration/components/favicon-test.ts
+++ web/tests/integration/components/favicon-test.ts
@@ -1,7 +1,10 @@
 import { module, test } from "qunit";
 import { setupRenderingTest } from "ember-qunit";
-import { render } from "@ember/test-helpers";
+import { find, render, waitFor } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
+
+const FALLBACK_ICON_SELECTOR = "[data-test-fallback-favicon]";
+const FAVICON_SELECTOR = "[data-test-favicon]";
 
 module("Integration | Component | favicon", function (hooks) {
   setupRenderingTest(hooks);
@@ -12,10 +15,25 @@ module("Integration | Component | favicon", function (hooks) {
     `);
 
     assert
-      .dom("[data-test-favicon]")
+      .dom(FAVICON_SELECTOR)
       .hasAttribute(
         "src",
-        "https://www.google.com/s2/favicons?domain=https://hashicorp.com&sz=32"
+        "https://www.google.com/s2/favicons?sz=64&domain=https://hashicorp.com"
       );
+  });
+
+  test("it shows a fallback icon when the favicon URL is invalid", async function (assert) {
+    await render(hbs`
+      <Favicon @url="-"/>
+    `);
+
+    assert.dom(FALLBACK_ICON_SELECTOR).doesNotExist();
+
+    find(FAVICON_SELECTOR)?.dispatchEvent(new Event("error"));
+
+    await waitFor(FALLBACK_ICON_SELECTOR);
+
+    assert.dom(FALLBACK_ICON_SELECTOR).exists();
+    assert.dom(FAVICON_SELECTOR).doesNotExist();
   });
 });
