6d8376bc1ea028fb3f035eb076a22c5c3343c35d
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-08-29 11:44:42 -0400

    "New" menu design

--- web/app/components/nav/create-button.ts
+++ web/app/components/nav/create-button.ts
@@ -10,40 +10,46 @@ export default class NavNewCreateButton extends Component<NavNewCreateButtonSign
     return [
       {
         label: "Request for comments",
+        icon: "discussion-circle",
         shortName: "RFC",
         description: "Ask colleagues for feedback on a proposal.",
       },
       {
         label: "Product requirements",
+        icon: "target",
         shortName: "PRD",
         description: "Plan a new product or feature.",
       },
       {
         label: "Funding request",
+        icon: "dollar-sign",
         shortName: "FRD",
         description: "Get buy-in for your project's budget.",
       },
       {
         label: "Plan of record",
+        icon: "map",
         shortName: "POR",
         description: "Set goals and milestones for an initiative.",
       },
       {
-        label: "Memo",
-        shortName: "MEMO",
-        description: "Share freeform info with your team.",
-      },
-      {
         label: "Press release / FAQ",
+        icon: "newspaper",
         shortName: "PRFAQ",
         description: "Write a press release for your project.",
+      },
+      {
+        label: "Memo",
+        icon: "radio",
+        shortName: "MEMO",
+        description: "Share freeform info with your team.",
       },
     ];
   }
 
   protected get menuItems() {
     const projectItem = {
-      label: "Create a project",
+      label: "Start a project",
       shortName: "PROJECT",
       description: "Create a new project.",
     };
