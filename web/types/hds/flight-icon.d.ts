0ccfd1c504e3c6b91b07492a5266f2efc5e7997c
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-10-09 11:18:41 -0400

    Remove unused change

--- web/types/hds/flight-icon.d.ts
+++ web/types/hds/flight-icon.d.ts
@@ -1,19 +1,13 @@
-declare module "@hashicorp/ember-flight-icons/components/flight-icon" {
-  import Component from "@glimmer/component";
-  import { ComponentLike } from "@glint/template";
+// https://helios.hashicorp.design/icons/usage-guidelines?tab=code
 
-  // https://helios.hashicorp.design/icons/usage-guidelines?tab=code
+import { ComponentLike } from "@glint/template";
 
-  interface FlightIconComponentSignature {
-    Element: SVGElement;
-    Args: {
-      name: string;
-      size?: "16" | "24";
-      color?: string;
-      stretched?: boolean;
-    };
-  }
-
-  export type FlightIconComponent = ComponentLike<FlightIconComponentSignature>;
-  export default class FlightIcon extends Component<FlightIconComponentSignature> {}
-}
+export type FlightIconComponent = ComponentLike<{
+  Element: SVGElement;
+  Args: {
+    name: string;
+    size?: "16" | "24";
+    color?: string;
+    stretched?: boolean;
+  };
+}>;
