d94679e8466e04e24d7e240fab20f462f2a50ded
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-10-11 19:48:42 -0400

    Remove masonry package

--- web/app/components/project/list.ts
+++ web/app/components/project/list.ts
@@ -2,7 +2,6 @@ import { action } from "@ember/object";
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import { HermesProject } from "hermes/routes/authenticated/projects";
-import Masonry from "masonry-layout";
 
 interface ProjectListComponentSignature {
   Args: {
@@ -12,19 +11,6 @@ interface ProjectListComponentSignature {
 
 export default class ProjectListComponent extends Component<ProjectListComponentSignature> {
   @tracked private container: HTMLElement | null = null;
-  // we may want some "sortedProjects" computed property here
-  @action protected configureMasonry(element: HTMLElement): void {
-    this.container = element;
-    const masonry = new Masonry(this.container, {
-      itemSelector: ".project-tile",
-      columnWidth: ".project-tile",
-      percentPosition: true,
-      gutter: 10,
-      transitionDuration: 0,
-    });
-
-    masonry.layout;
-  }
 }
 
 declare module "@glint/environment-ember-loose/registry" {
