06f8418b7af89c89a67e1da6f5ba16794dad33ea
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-10-19 11:01:35 -0400

    Improve EditableField error handling

---
+++ web/app/utils/blink-element.ts
@@ -0,0 +1,21 @@
+/**
+ * Blinks an element twice by toggling its visibility.
+ * Used for emphasis, such as to reiterate an
+ * already-visible form error.
+ */
+export default function blinkElement(element?: Element | null) {
+  if (!element) {
+    return;
+  }
+
+  for (let i = 0; i < 4; i++) {
+    // Alternate between hidden and visible
+    let visibility = i % 2 === 0 ? "hidden" : "visible";
+
+    setTimeout(() => {
+      if (element) {
+        element.setAttribute("style", `visibility: ${visibility}`);
+      }
+    }, i * 100);
+  }
+}
