34dafd3d304e6e261de203964c89756e2eb06e5c
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-10-20 11:36:17 -0400

    Switch DocThumbnail sizing interface from `isLarge` to `size` (#378)
    Switch from `isLarge` to `size`

--- web/app/components/doc/folder-affordance.ts
+++ web/app/components/doc/folder-affordance.ts
@@ -2,11 +2,15 @@ import Component from "@glimmer/component";
 
 interface DocFolderAffordanceSignature {
   Args: {
-    isLarge?: boolean;
+    size?: "large";
   };
 }
 
-export default class DocFolderAffordance extends Component<DocFolderAffordanceSignature> {}
+export default class DocFolderAffordance extends Component<DocFolderAffordanceSignature> {
+  protected get sizeIsLarge(): boolean {
+    return this.args.size === "large";
+  }
+}
 
 declare module "@glint/environment-ember-loose/registry" {
   export default interface Registry {
