e1d0c47307ff51211f01064cca9dd5626e1c0645
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-10-25 21:34:58 -0400

    Add `medium` size to DocThumbnail

--- web/app/components/doc/folder-affordance.ts
+++ web/app/components/doc/folder-affordance.ts
@@ -1,12 +1,21 @@
 import Component from "@glimmer/component";
+import { DocThumbnailSize } from "hermes/components/doc/thumbnail";
 
 interface DocFolderAffordanceSignature {
   Args: {
-    size?: "large";
+    size?: `${DocThumbnailSize}`;
   };
 }
 
 export default class DocFolderAffordance extends Component<DocFolderAffordanceSignature> {
+  protected get sizeIsSmall(): boolean {
+    return this.args.size === "small" || !this.args.size;
+  }
+
+  protected get sizeIsMedium(): boolean {
+    return this.args.size === "medium";
+  }
+
   protected get sizeIsLarge(): boolean {
     return this.args.size === "large";
   }
--- web/app/components/doc/thumbnail.ts
+++ web/app/components/doc/thumbnail.ts
@@ -1,17 +1,28 @@
 import Component from "@glimmer/component";
 import { dasherize } from "@ember/string";
 import getProductId from "hermes/utils/get-product-id";
+import { inject as service } from "@ember/service";
+import ProductAreasService from "hermes/services/product-areas";
+
+export enum DocThumbnailSize {
+  Small = "small",
+  Medium = "medium",
+  Large = "large",
+}
 
 interface DocThumbnailComponentSignature {
   Element: HTMLDivElement;
   Args: {
     status?: string;
     product?: string;
-    size?: "large";
+    badgeIsHidden?: boolean;
+    size?: `${DocThumbnailSize}`;
   };
 }
 
 export default class DocThumbnailComponent extends Component<DocThumbnailComponentSignature> {
+  @service declare productAreas: ProductAreasService;
+
   protected get status(): string | null {
     if (this.args.status) {
       return dasherize(this.args.status);
@@ -20,16 +31,26 @@ export default class DocThumbnailComponent extends Component<DocThumbnailCompone
     }
   }
 
-  protected get sizeIsLarge(): boolean {
-    return this.args.size === "large";
+  protected get productAvatarSize() {
+    if (this.sizeIsLarge) {
+      return "large";
+    } else if (this.sizeIsMedium) {
+      return "medium";
+    } else {
+      return "small";
+    }
   }
 
-  protected get productShortName(): string | null {
-    if (this.args.product) {
-      return getProductId(this.args.product);
-    } else {
-      return null;
-    }
+  protected get sizeIsSmall(): boolean {
+    return this.args.size === "small" || !this.args.size;
+  }
+
+  protected get sizeIsMedium(): boolean {
+    return this.args.size === "medium";
+  }
+
+  protected get sizeIsLarge(): boolean {
+    return this.args.size === "large";
   }
 
   protected get isApproved(): boolean {
@@ -38,6 +59,22 @@ export default class DocThumbnailComponent extends Component<DocThumbnailCompone
 
   protected get isObsolete(): boolean {
     return this.status === "obsolete";
+  }
+
+  protected get badgeIsShown(): boolean {
+    if (this.args.badgeIsHidden) {
+      return false;
+    }
+
+    if (getProductId(this.args.product)) {
+      return true;
+    }
+
+    if (this.productAreas.getAbbreviation(this.args.product)) {
+      return true;
+    }
+
+    return false;
   }
 }
 
