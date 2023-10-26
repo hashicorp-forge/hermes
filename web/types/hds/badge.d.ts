4c93e3915d124cf8ada7d61b10729c6710892f16
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-10-25 21:07:43 -0400

    Tweak HDS types

--- web/types/hds/badge.d.ts
+++ web/types/hds/badge.d.ts
@@ -1,16 +1,12 @@
 // https://helios.hashicorp.design/components/badge?tab=code#component-api
 
 import { ComponentLike } from "@glint/template";
-import {
-  HdsBadgeColor,
-  HdsBadgeType,
-  HdsComponentSize,
-} from "hds/_shared";
+import { HdsBadgeColor, HdsBadgeType, HdsComponentSize } from "hds/_shared";
 
 interface HdsBadgeComponentSignature {
   Element: HTMLDivElement;
   Args: {
-    text: string;
+    text: string | number;
     color?: HdsBadgeColor;
     type?: HdsBadgeType;
     size?: HdsComponentSize;
