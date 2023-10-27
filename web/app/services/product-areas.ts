35ceb23e61ee5440b50a51f142ace7fa14012408
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-10-27 15:52:28 -0400

    Start of expanded ProductAreasService

--- web/app/services/product-areas.ts
+++ web/app/services/product-areas.ts
@@ -3,6 +3,7 @@ import { tracked } from "@glimmer/tracking";
 import { task } from "ember-concurrency";
 import ConfigService from "hermes/services/config";
 import FetchService from "./fetch";
+import { assert } from "@ember/debug";
 
 export type ProductArea = {
   abbreviation: string;
@@ -12,15 +13,34 @@ export default class ProductAreasService extends Service {
   @service("config") declare configSvc: ConfigService;
   @service("fetch") declare fetchSvc: FetchService;
 
-  @tracked index: Record<string, ProductArea> | null = null;
+  @tracked _index: Record<string, ProductArea> | null = null;
+
+  get index(): Record<string, ProductArea> {
+    assert("_index must exist", this._index);
+    return this._index;
+  }
+
+  getAbbreviation(productName?: string): string | undefined {
+    if (!productName) {
+      return;
+    }
+
+    const product = this.index[productName];
+
+    if (!product) {
+      return;
+    }
+
+    return product.abbreviation.slice(0, 3).toUpperCase();
+  }
 
   fetch = task(async () => {
     try {
-      this.index = await this.fetchSvc
+      this._index = await this.fetchSvc
         .fetch(`/api/${this.configSvc.config.api_version}/products`)
         .then((resp) => resp?.json());
     } catch (err) {
-      this.index = null;
+      this._index = null;
       throw err;
     }
   });
