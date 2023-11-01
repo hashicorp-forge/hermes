6c3c3f193a1ec894beb9f4be53ef5fb6262e4541
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-10-31 20:33:31 -0400

    Add XL to PersonAvatar

--- web/app/types/avatar-size.ts
+++ web/app/types/avatar-size.ts
@@ -1,5 +1,13 @@
-export enum HermesAvatarSize {
+export enum HermesBasicAvatarSize {
   Small = "small",
   Medium = "medium",
   Large = "large",
 }
+
+export enum HermesExtendedAvatarSize {
+  XL = "xl",
+}
+
+export type HermesPersonAvatarSize =
+  | HermesBasicAvatarSize
+  | HermesExtendedAvatarSize;
