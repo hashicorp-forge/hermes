7ce577497e590c09d72798acefa2bef4fa61fd42
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-11-17 12:10:45 -0500

    Remove `initialize` function

--- web/app/services/flags.ts
+++ web/app/services/flags.ts
@@ -1,5 +1,4 @@
 import Service, { inject as service } from "@ember/service";
-import config from "hermes/config/environment";
 import ConfigService from "./config";
 
 export default class FlagsService extends Service {
@@ -7,10 +6,5 @@ export default class FlagsService extends Service {
 
   get projects() {
     return this.configSvc.config.feature_flags["projects"];
-  }
-
-  initialize() {
-    // @ts-ignore
-    this.setProperties(config.featureFlags);
   }
 }
