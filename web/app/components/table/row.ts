9f1888f7502e4cfe32683ca720dd1d8eca460e52
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-11-21 18:08:13 -0500

    Add `timeColumn` argument to `Table::Row` (#423)
    * Edit "time-ago" function
    
    * Make limiting optional
    
    * Prep for `modifiedTime` column support
    
    * Tests and cleanup
    
    * Update row-test.ts
    
    * ElseIf tweak

--- web/app/components/table/row.ts
+++ web/app/components/table/row.ts
@@ -1,17 +1,39 @@
 import Component from "@glimmer/component";
 import { HermesDocument } from "hermes/types/document";
-import { inject as service } from "@ember/service";
-import AuthenticatedUserService from "hermes/services/authenticated-user";
-import parseDate from "hermes/utils/parse-date";
+import timeAgo from "hermes/utils/time-ago";
+
+export enum TimeColumn {
+  Modified = "modifiedTime",
+  Created = "createdTime",
+}
 
 interface TableRowComponentSignature {
+  Element: HTMLTableRowElement;
   Args: {
     doc: HermesDocument;
+    timeColumn: `${TimeColumn}`;
   };
 }
 
 export default class TableRowComponent extends Component<TableRowComponentSignature> {
-  @service declare authenticatedUser: AuthenticatedUserService;
+  protected get time() {
+    const { modifiedTime, createdTime } = this.args.doc;
+    const { timeColumn } = this.args;
+
+    let time = null;
+
+    if (modifiedTime && timeColumn === TimeColumn.Modified) {
+      time = modifiedTime;
+    } else if (createdTime && timeColumn === TimeColumn.Created) {
+      time = createdTime;
+    }
+
+    if (time) {
+      return timeAgo(time, { limitTo24Hours: true });
+    }
+
+    return "Unknown";
+  }
 
   protected get isDraft() {
     if (this.args.doc.isDraft) {
@@ -19,21 +41,6 @@ export default class TableRowComponent extends Component<TableRowComponentSignat
     }
 
     return this.args.doc.status === "WIP";
-  }
-
-  protected get ownerIsAuthenticatedUser() {
-    const docOwner = this.args.doc.owners?.[0];
-
-    if (!docOwner) {
-      return false;
-    }
-
-    return docOwner === this.authenticatedUser.info.email;
-  }
-
-  protected get time() {
-    const { created } = this.args.doc;
-    return parseDate(created) as string;
   }
 }
 
