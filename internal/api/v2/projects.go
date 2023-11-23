1b7232c6a8acb2d6067a4f9ef64b8de28ac0c215
Josh Freda <jfreda@users.noreply.github.com>
2023-11-22 18:30:57 -0600

    Save projects in Algolia (#447)
    * Add projects Algolia index
    
    * Save projects in Algolia
    
    * Fix 'completed' status in PATCH request
    
    * Add projects feature flag in config

--- internal/api/v2/projects.go
+++ internal/api/v2/projects.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 
 	"github.com/hashicorp-forge/hermes/internal/server"
+	"github.com/hashicorp-forge/hermes/pkg/algolia"
 	"github.com/hashicorp-forge/hermes/pkg/models"
 	"gorm.io/gorm"
 )
@@ -177,6 +178,19 @@ func ProjectsHandler(srv server.Server) http.Handler {
 
 			srv.Logger.Info("created project", logArgs...)
 
+			// Request post-processing.
+			go func() {
+				// Save project in Algolia.
+				if err := saveProjectInAlgolia(proj, srv.AlgoWrite); err != nil {
+					srv.Logger.Error("error saving project in Algolia",
+						append([]interface{}{
+							"error", err,
+						}, logArgs...)...,
+					)
+					return
+				}
+			}()
+
 		default:
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
@@ -303,11 +317,11 @@ func ProjectHandler(srv server.Server) http.Handler {
 					switch strings.ToLower(*req.Status) {
 					case "active":
 					case "archived":
-					case "complete":
+					case "completed":
 					default:
 						http.Error(w,
 							"Bad request: invalid status"+
-								` (valid values are "active", "archived", "complete")`,
+								` (valid values are "active", "archived", "completed")`,
 							http.StatusBadRequest)
 						return
 					}
@@ -354,7 +368,7 @@ func ProjectHandler(srv server.Server) http.Handler {
 						patch.Status = models.ActiveProjectStatus
 					case "archived":
 						patch.Status = models.ArchivedProjectStatus
-					case "complete":
+					case "completed":
 						patch.Status = models.CompletedProjectStatus
 					}
 				}
@@ -374,6 +388,19 @@ func ProjectHandler(srv server.Server) http.Handler {
 				}
 
 				srv.Logger.Info("updated project", logArgs...)
+
+				// Request post-processing.
+				go func() {
+					// Save project in Algolia.
+					if err := saveProjectInAlgolia(patch, srv.AlgoWrite); err != nil {
+						srv.Logger.Error("error saving project in Algolia",
+							append([]interface{}{
+								"error", err,
+							}, logArgs...)...,
+						)
+						return
+					}
+				}()
 
 			default:
 				w.WriteHeader(http.StatusMethodNotAllowed)
@@ -408,4 +435,34 @@ func getProjectIDFromPath(path string, re *regexp.Regexp) (uint, error) {
 	}
 
 	return uint(projectID), nil
+}
+
+// saveProjectInAlgolia saves a project in Algolia.
+func saveProjectInAlgolia(
+	proj models.Project,
+	algoClient *algolia.Client,
+) error {
+	// Convert project to Algolia object.
+	projObj := map[string]any{
+		"createdTime":  proj.ProjectCreatedAt.Unix(),
+		"creator":      proj.Creator.EmailAddress,
+		"description":  proj.Description,
+		"jiraIssueID":  proj.JiraIssueID,
+		"modifiedTime": proj.ProjectModifiedAt.Unix(),
+		"objectID":     fmt.Sprintf("%d", proj.ID),
+		"status":       proj.Status.ToString(),
+		"title":        proj.Title,
+	}
+
+	// Save project in Algolia.
+	res, err := algoClient.Projects.SaveObject(projObj)
+	if err != nil {
+		return fmt.Errorf("error saving object: %w", err)
+	}
+	err = res.Wait()
+	if err != nil {
+		return fmt.Errorf("error waiting for save: %w", err)
+	}
+
+	return nil
 }
