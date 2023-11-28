0215d52041f54ef50b332b9914bf990c79688efc
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-11-28 13:58:52 -0500

    Cleanup

--- web/app/routes/authenticated/projects/index.ts
+++ web/app/routes/authenticated/projects/index.ts
@@ -9,9 +9,6 @@ export default class AuthenticatedProjectsIndexRoute extends Route {
   @service("config") declare configSvc: ConfigService;
 
   async model(): Promise<BackEndHermesProject[]> {
-    // FIXME: May need a service to cache the data
-    // FIXME: Try skeleton screen if loading remains slow
-
     return await this.fetchSvc
       .fetch(`/api/${this.configSvc.config.api_version}/projects`)
       .then((response) => response?.json());
--- web/app/routes/authenticated/projects/project.ts
+++ web/app/routes/authenticated/projects/project.ts
@@ -1,9 +1,7 @@
 import Route from "@ember/routing/route";
 import { inject as service } from "@ember/service";
-import { RelatedHermesDocument } from "hermes/components/related-resources";
 import ConfigService from "hermes/services/config";
 import FetchService from "hermes/services/fetch";
-import { HermesDocument } from "hermes/types/document";
 import { HermesProject } from "hermes/types/project";
 
 export default class AuthenticatedProjectsProjectRoute extends Route {
@@ -28,35 +26,7 @@ export default class AuthenticatedProjectsProjectRoute extends Route {
       projectResourcesPromise,
     ]);
 
-    let { hermesDocuments, externalLinks } = projectResources;
-
-    let documentPromises: Promise<HermesDocument>[] = [];
-
-    hermesDocuments?.forEach((doc: RelatedHermesDocument) => {
-      documentPromises.push(
-        this.fetchSvc
-          .fetch(
-            `/api/${this.configSvc.config.api_version}/documents/${doc.googleFileID}`,
-          )
-          .then((response) => response?.json()),
-      );
-    });
-
-    const documents: HermesDocument[] = await Promise.all(documentPromises);
-
-    hermesDocuments = hermesDocuments?.map((doc: RelatedHermesDocument) => {
-      const document = documents.find((d) => d.objectID === doc.googleFileID);
-
-      return {
-        ...doc,
-        summary: document?.summary,
-        product: document?.product,
-        status: document?.status,
-        owners: document?.owners,
-        docType: document?.docType,
-        ownerPhotos: document?.ownerPhotos,
-      };
-    }) as RelatedHermesDocument[];
+    const { hermesDocuments, externalLinks } = projectResources;
 
     return {
       ...project,
