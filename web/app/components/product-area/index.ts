01cc8637af0a078e801453c39a2138039a4cccea
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-11-30 20:32:04 -0500

    Remove unused imports

--- web/app/components/product-area/index.ts
+++ web/app/components/product-area/index.ts
@@ -1,8 +1,5 @@
-import { inject as service } from "@ember/service";
 import Component from "@glimmer/component";
 import { HITS_PER_PAGE } from "hermes/services/algolia";
-import AuthenticatedUserService from "hermes/services/authenticated-user";
-import FlagsService from "hermes/services/flags";
 import { HermesDocument } from "hermes/types/document";
 
 interface ProductAreaIndexComponentSignature {
@@ -14,9 +11,6 @@ interface ProductAreaIndexComponentSignature {
 }
 
 export default class ProductAreaIndexComponent extends Component<ProductAreaIndexComponentSignature> {
-  @service declare authenticatedUser: AuthenticatedUserService;
-  @service declare flags: FlagsService;
-
   protected get seeMoreButtonIsShown() {
     return this.args.nbHits > HITS_PER_PAGE;
   }
