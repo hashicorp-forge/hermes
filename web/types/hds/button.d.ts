573b69e64896afbf19f39f05cd048f6dda267d25
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-11-30 20:19:57 -0500

    Revert change

--- web/types/hds/button.d.ts
+++ web/types/hds/button.d.ts
@@ -1,26 +1,23 @@
 // https://helios.hashicorp.design/components/button?tab=code#component-api
-declare module "@hashicorp/design-system-components/components/hds/button" {
-  import Component from "@glimmer/component";
-  import { ComponentLike } from "@glint/template";
-  import {
-    HdsButtonColor,
-    HdsIconPosition,
-    HdsComponentSize,
-    HdsAnchorComponentArgs,
-  } from "hds/_shared";
 
-  interface HdsButtonComponentSignature {
-    Element: HTMLButtonElement;
-    Args: HdsAnchorComponentArgs & {
-      text: string;
-      size?: HdsComponentSize;
-      color?: HdsButtonColor;
-      isIconOnly?: boolean;
-      isFullWidth?: boolean;
-      isRouteExternal?: boolean;
-    };
-  }
+import { ComponentLike } from "@glint/template";
+import {
+  HdsButtonColor,
+  HdsIconPosition,
+  HdsComponentSize,
+  HdsAnchorComponentArgs,
+} from "hds/_shared";
 
-  export type HdsButtonComponent = ComponentLike<HdsButtonComponentSignature>;
-  export default class HdsButton extends Component<HdsButtonComponentSignature> {}
+interface HdsButtonComponentSignature {
+  Element: HTMLButtonElement;
+  Args: HdsAnchorComponentArgs & {
+    text: string;
+    size?: HdsComponentSize;
+    color?: HdsButtonColor;
+    isIconOnly?: boolean;
+    isFullWidth?: boolean;
+    isRouteExternal?: boolean;
+  };
 }
+
+export type HdsButtonComponent = ComponentLike<HdsButtonComponentSignature>;
