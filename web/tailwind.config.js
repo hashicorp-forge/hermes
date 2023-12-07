228661fd327efe2519b50850e4d5870516cbfade
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-12-07 14:59:37 -0500

    Update tailwind.config.js

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -186,15 +186,9 @@ module.exports = {
 
         // Non-Semantic Color
         "color-palette-blue-200": "var(--token-color-palette-blue-200)",
-        "color-palette-green-100": "var(--token-color-palette-green-100)",
         "color-palette-green-200": "var(--token-color-palette-green-200)",
-        "color-palette-green-300": "var(--token-color-palette-green-300)",
-        "color-palette-green-400": "var(--token-color-palette-green-400)",
         "color-palette-purple-100": "var(--token-color-palette-purple-100)",
         "color-palette-purple-200": "var(--token-color-palette-purple-200)",
-        "color-palette-amber-100": "var(--token-color-palette-amber-100)",
-        "color-palette-amber-200": "var(--token-color-palette-amber-200)",
-        "color-palette-amber-300": "var(--token-color-palette-amber-300)",
       },
     },
   },
