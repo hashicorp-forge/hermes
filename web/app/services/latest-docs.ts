09db19739f0e73e41c5237c0fb38e5121cd2433b
Jeff Daley <jeffdaley@users.noreply.github.com>
2023-12-06 19:46:58 -0500

    Cleanup and documentation

--- web/app/services/latest-docs.ts
+++ web/app/services/latest-docs.ts
@@ -1,6 +1,6 @@
 import Service from "@ember/service";
 import { inject as service } from "@ember/service";
-import { keepLatestTask, timeout } from "ember-concurrency";
+import { keepLatestTask } from "ember-concurrency";
 import { tracked } from "@glimmer/tracking";
 import ConfigService from "hermes/services/config";
 import { HermesDocument } from "hermes/types/document";
@@ -10,12 +10,24 @@ import AlgoliaService from "./algolia";
 export default class LatestDocsService extends Service {
   @service("config") declare configSvc: ConfigService;
   @service declare algolia: AlgoliaService;
-  @service declare session: any;
 
+  /**
+   * The index of the most recent documents.
+   * Used by the dashboard feed.
+   */
   @tracked index: HermesDocument[] | null = null;
 
+  /**
+   * The number of pages of results. If this is more than 1,
+   * a "See all" link will be shown on the dashboard.
+   */
   @tracked nbPages = 0;
 
+  /**
+   * The call to fetch the latest documents.
+   * Awaited by the dashboard on first load then updated
+   * in the background on subsequent visits.
+   */
   fetchAll = keepLatestTask(async () => {
     const response = await this.algolia.searchIndex
       .perform(
