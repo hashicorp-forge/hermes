bf7c16de005b20987bad24d495e806d80f66aa0a
Jeff Daley <jeffdaley@users.noreply.github.com>
2024-01-22 09:55:22 -0500

    Add name

--- web/app/models/person.ts
+++ web/app/models/person.ts
@@ -2,6 +2,7 @@ import Model, { attr } from "@ember-data/model";
 
 export default class PersonModel extends Model {
   @attr declare name: string;
+  @attr declare firstName: string;
   @attr declare email: string;
   @attr declare picture: string;
 }
--- web/app/serializers/person.ts
+++ web/app/serializers/person.ts
@@ -14,7 +14,8 @@ export default class PersonSerializer extends JSONSerializer {
         id: payload[0].emailAddresses[0].value, // so it can be queried
         type: primaryModelClass.modelName,
         attributes: {
-          // name: payload[0].names[0].displayName,
+          name: payload[0].names[0].displayName,
+          firstName: payload[0].names[0].givenName,
           email: payload[0].emailAddresses[0].value,
           picture: payload[0].photos[0].url,
         },
