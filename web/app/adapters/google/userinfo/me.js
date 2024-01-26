4693b57569f624fd470cf19323cb2791f1174457
Jeff Daley <jeffdaley@users.noreply.github.com>
2024-01-26 09:39:11 -0500

    Remove unused EmberData files

--- web/app/adapters/google/userinfo/me.js
+++
@@ -1,8 +0,0 @@
-import GoogleUserinfoAdapter from "../userinfo";
-
-export default class GoogleUserinfoMeAdapter extends GoogleUserinfoAdapter {
-  urlForQueryRecord(query, modelName) {
-    let baseUrl = this.buildURL();
-    return `${baseUrl}/me`;
-  }
-}
