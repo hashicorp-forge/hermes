bbbe90c68b98c75921a67be7bffd838a5141b577
Jeff Daley <jeffdaley@users.noreply.github.com>
2024-02-27 09:24:01 -0500

    Fix /documents filters

--- web/app/controllers/authenticated/documents.ts
+++ web/app/controllers/authenticated/documents.ts
@@ -2,6 +2,7 @@ import Controller from "@ember/controller";
 import { SortByValue } from "hermes/components/header/toolbar";
 import { SortDirection } from "hermes/components/table/sortable-header";
 import AuthenticatedDocumentsRoute from "hermes/routes/authenticated/documents";
+import { SearchScope } from "hermes/routes/authenticated/results";
 import { ModelFrom } from "hermes/types/route-models";
 
 export default class AuthenticatedDocumentsController extends Controller {
@@ -12,6 +13,8 @@ export default class AuthenticatedDocumentsController extends Controller {
   product = [];
   sortBy = "dateDesc";
   status = [];
+
+  scope = SearchScope.Docs;
 
   declare model: ModelFrom<AuthenticatedDocumentsRoute>;
 
