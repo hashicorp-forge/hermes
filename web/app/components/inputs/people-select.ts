8b090e0ab9cd44e26fc51e3c84e7a632acaa3513
Jeff Daley <jeffdaley@users.noreply.github.com>
2024-03-05 17:21:11 -0500

    Add offset, improve styles

--- web/app/components/inputs/people-select.ts
+++ web/app/components/inputs/people-select.ts
@@ -8,11 +8,37 @@ import FetchService from "hermes/services/fetch";
 import Ember from "ember";
 import StoreService from "hermes/services/store";
 import PersonModel from "hermes/models/person";
+import { Select } from "ember-power-select/components/power-select";
+import { next } from "@ember/runloop";
+import calculatePosition from "ember-basic-dropdown/utils/calculate-position";
+import { assert } from "@ember/debug";
 
 export interface GoogleUser {
   emailAddresses: { value: string }[];
   names: { displayName: string; givenName: string }[];
   photos: { url: string }[];
+}
+
+enum VerticalPosition {
+  Above = "above",
+  Below = "below",
+  Auto = "auto",
+}
+
+enum HorizontalPosition {
+  Left = "left",
+  Right = "right",
+  Auto = "auto",
+}
+
+interface CalculatePositionOptions {
+  horizontalPosition: HorizontalPosition;
+  verticalPosition: VerticalPosition;
+  matchTriggerWidth: boolean;
+  previousHorizontalPosition?: HorizontalPosition;
+  previousVerticalPosition?: VerticalPosition;
+  renderInPlace: boolean;
+  dropdown: any;
 }
 
 interface InputsPeopleSelectComponentSignature {
@@ -41,14 +67,38 @@ export default class InputsPeopleSelectComponent extends Component<InputsPeopleS
   @tracked protected people: string[] = [];
 
   /**
+   * The action to run when the PowerSelect input is clicked.
+   * Prevents the dropdown from opening when the input is empty.
+   */
+  @action protected onClick(e: MouseEvent) {
+    const target = e.target as HTMLElement;
+    const input = target.closest(".ember-power-select-trigger") as HTMLElement;
+
+    if (input) {
+      const value = input.querySelector("input")?.value;
+      if (value === "") {
+        e.stopImmediatePropagation();
+      }
+    }
+  }
+
+  /**
    * An action occurring on every keystroke.
    * Handles cases where the user clears the input,
    * since `onChange` is not called in that case.
    * See: https://ember-power-select.com/docs/custom-search-action
    */
-  @action onInput(inputValue: string) {
+  @action protected onInput(inputValue: string, select: Select) {
     if (inputValue === "") {
       this.people = [];
+
+      /**
+       * Stop the redundant "type to search" message
+       * from appearing when the last character is deleted.
+       */
+      next(() => {
+        select.actions.close();
+      });
     }
   }
 
@@ -58,6 +108,45 @@ export default class InputsPeopleSelectComponent extends Component<InputsPeopleS
    */
   @action onClose() {
     this.people = [];
+  }
+
+  /**
+   * Custom position-calculating function for the dropdown.
+   * Ensures the dropdown is at least 320px wide, instead of 100% of the trigger.
+   * Improves dropdown appearance when the trigger is small, e.g., in the sidebar.
+   */
+  @action protected calculatePosition(
+    trigger: HTMLElement,
+    content: HTMLElement,
+    destination: HTMLElement,
+    options: CalculatePositionOptions,
+  ) {
+    const position = calculatePosition(trigger, content, destination, options);
+
+    const extraOffsetBelow = 4;
+    const extraOffsetAbove = extraOffsetBelow + 2;
+
+    const { verticalPosition } = position;
+
+    console.log("verticalPosition", verticalPosition);
+
+    let { top } = position.style;
+
+    assert("top must be a number", typeof top === "number");
+
+    switch (verticalPosition) {
+      case VerticalPosition.Above:
+        top -= extraOffsetAbove;
+        break;
+      case VerticalPosition.Below:
+        top += extraOffsetBelow;
+        break;
+    }
+
+    position.style.top = top;
+    position.style["min-width"] = `320px`;
+
+    return position;
   }
 
   /**
