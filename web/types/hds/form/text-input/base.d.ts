5ee787dca9241337998df44d0fb3680d5565ce07
Jeff Daley <jeffdaley@users.noreply.github.com>
2024-03-11 16:44:04 -0400

    Update base.d.ts

--- web/types/hds/form/text-input/base.d.ts
+++ web/types/hds/form/text-input/base.d.ts
@@ -1,22 +1,17 @@
 // https://helios.hashicorp.design/components/form/text-input?tab=code#formtextinputbase-1
 
-declare module "@hashicorp/design-system-components/components/hds/form/text-input/base" {
-  import Component from "@glimmer/component";
-  import { ComponentLike } from "@glint/template";
-  import { HdsFormTextInputArgs } from ".";
+import { ComponentLike } from "@glint/template";
+import { HdsFormTextInputArgs } from ".";
 
-  export interface HdsFormTextInputBaseComponentSignature {
-    Element: HTMLInputElement;
-    Args: {
-      type?: string;
-      value: string | number | Date;
-      isInvalid?: boolean;
-      width?: string;
-    };
-  }
-
-  export type HdsFormTextInputBaseComponent =
-    ComponentLike<HdsFormTextInputBaseComponentSignature>;
-
-  export default class HdsFormTextInputBase extends Component<HdsFormTextInputBaseComponentSignature> {}
+export interface HdsFormTextInputBaseComponentSignature {
+  Element: HTMLInputElement;
+  Args: {
+    type?: string;
+    value: string | number | Date;
+    isInvalid?: boolean;
+    width?: string;
+  };
 }
+
+export type HdsFormTextInputBaseComponent =
+  ComponentLike<HdsFormTextInputBaseComponentSignature>;
