d88904031ad8c6c8b50425354d7abb628c57f986
Jeff Daley <jeffdaley@users.noreply.github.com>
2024-03-13 17:20:40 -0400

    Filter inaccessible docs out of Recently Viewed (#646)
    Handle errors in Recently Viewed

--- web/tests/unit/services/recently-viewed-test.ts
+++ web/tests/unit/services/recently-viewed-test.ts
@@ -2,6 +2,7 @@ import { module, test } from "qunit";
 import { setupTest } from "ember-qunit";
 import RecentlyViewedService from "hermes/services/recently-viewed";
 import { MirageTestContext, setupMirage } from "ember-cli-mirage/test-support";
+import { TEST_USER_2_EMAIL } from "hermes/mirage/utils";
 
 interface Context extends MirageTestContext {
   recentlyViewed: RecentlyViewedService;
@@ -27,6 +28,25 @@ module("Unit | Service | recently-viewed", function (hooks) {
       this.recentlyViewed.index?.length,
       10,
       "the index is populated",
+    );
+  });
+
+  test("it ignores errors when fetching the index", async function (this: Context, assert) {
+    // Create a document that the user does not have access to
+    this.server.create("document", { id: 1, owners: [TEST_USER_2_EMAIL] });
+    this.server.create("recently-viewed-doc", {
+      id: 1,
+    });
+
+    // Create documents that the user has access to
+    this.server.createList("recently-viewed-doc", 5);
+
+    await this.recentlyViewed.fetchAll.perform();
+
+    assert.equal(
+      this.recentlyViewed.index?.length,
+      5,
+      "the inaccessible document is ignored",
     );
   });
 });
