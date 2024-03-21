15e951c02f6e3dd406b5451abc275352ee4402b4
Jeff Daley <jeffdaley@users.noreply.github.com>
2024-03-21 16:24:03 -0400

    Add headline to filtered docs

--- web/app/controllers/authenticated/documents.ts
+++ web/app/controllers/authenticated/documents.ts
@@ -1,10 +1,14 @@
 import Controller from "@ember/controller";
+import { inject as service } from "@ember/service";
 import { SortByValue } from "hermes/components/header/toolbar";
 import { SortDirection } from "hermes/components/table/sortable-header";
 import AuthenticatedDocumentsRoute from "hermes/routes/authenticated/documents";
+import ActiveFiltersService from "hermes/services/active-filters";
 import { ModelFrom } from "hermes/types/route-models";
 
 export default class AuthenticatedDocumentsController extends Controller {
+  @service declare activeFilters: ActiveFiltersService;
+
   queryParams = ["docType", "owners", "page", "product", "sortBy", "status"];
 
   docType = [];
