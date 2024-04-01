9dd33fc293bda5f8c8dd53ab9c5d46eee1bd7699
Jeff Daley <jeffdaley@users.noreply.github.com>
2024-04-01 17:04:54 -0400

    Remove out-of-scope prettier changes

--- web/app/utils/html-element.ts
+++ web/app/utils/html-element.ts
@@ -4,7 +4,7 @@ export default function htmlElement(selector: string): HTMLElement {
   const element = document.querySelector(selector);
   assert(
     "selector target must be an HTMLElement",
-    element instanceof HTMLElement,
+    element instanceof HTMLElement
   );
   return element;
 }
