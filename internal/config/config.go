9c72210b14529560bd966c363a10d911a5d9e71d
Josh Freda <jfreda@users.noreply.github.com>
2024-04-02 11:27:58 -0500

    Enable sending an email to users not found in the Google Workspace directory

--- internal/config/config.go
+++ internal/config/config.go
@@ -232,6 +232,10 @@ type GoogleWorkspace struct {
 	// create_docs_as_user is true in the auth block, so document notification
 	// settings will be the same as when a user creates their own document.
 	TemporaryDraftsFolder string `hcl:"temporary_drafts_folder,optional"`
+
+	// UserNotFoundEmail is the configuration to send an email when a user is not
+	// found in Google Workspace.
+	UserNotFoundEmail *GoogleWorkspaceUserNotFoundEmail `hcl:"user_not_found_email,block"`
 }
 
 // GoogleWorkspaceOAuth2 is the configuration to use OAuth 2.0 to access Google
@@ -247,6 +251,20 @@ type GoogleWorkspaceOAuth2 struct {
 	// RedirectURI is an authorized redirect URI for the given client_id as
 	// specified in the Google API Console Credentials page.
 	RedirectURI string `hcl:"redirect_uri,optional"`
+}
+
+// GoogleWorkspaceUserNotFoundEmail is the configuration to send an email when a
+// user is not found in Google Workspace.
+type GoogleWorkspaceUserNotFoundEmail struct {
+	// Body is the body of the email.
+	Body string `hcl:"body,optional"`
+
+	// Enabled enables sending an email when a user is not found in Google
+	// Workspace.
+	Enabled bool `hcl:"enabled,optional"`
+
+	// Subject is the subject of the email.
+	Subject string `hcl:"subject,optional"`
 }
 
 // Jira is the configuration for Hermes to work with Jira.
