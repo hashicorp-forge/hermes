812dcc15a6a762dafe1669d3bd5906f1ba78e6ee
Jacob Repp <jacobrepp@gmail.com>
2025-10-04 08:17:46 -0700

    feat(search): implement ProjectIndex and LinksIndex in Algolia adapter
    - Add projectsIndex and linksIndex fields to Adapter struct
    - Implement ProjectIndex() returning projectIndex wrapper
    - Implement LinksIndex() returning linksIndex wrapper
    - Add projectIndex type with Index, Delete, Search, GetObject, Clear methods
    - Add linksIndex type with SaveLink, DeleteLink, GetLink, Clear methods
    - Update NewAdapter() to initialize projects and links indexes from config
    - Add GetObject() implementation to documentIndex and draftIndex
    
    Algolia adapter now supports project indexing and document redirect links
    through the unified search.Provider interface.

--- pkg/search/adapters/algolia/adapter.go
+++ pkg/search/adapters/algolia/adapter.go
@@ -10,19 +10,25 @@ import (
 
 // Adapter implements search.Provider for Algolia.
 type Adapter struct {
-	client      *search.Client
-	docsIndex   *search.Index
-	draftsIndex *search.Index
-	appID       string
+	client        *search.Client
+	docsIndex     *search.Index
+	draftsIndex   *search.Index
+	projectsIndex *search.Index
+	linksIndex    *search.Index
+	appID         string
 }
 
 // Config contains Algolia configuration.
 type Config struct {
-	AppID           string
-	SearchAPIKey    string
-	WriteAPIKey     string
-	DocsIndexName   string
-	DraftsIndexName string
+	AppID                  string `hcl:"application_id,optional"`
+	SearchAPIKey           string `hcl:"search_api_key,optional"`
+	WriteAPIKey            string `hcl:"write_api_key,optional"`
+	DocsIndexName          string `hcl:"docs_index_name,optional"`
+	DraftsIndexName        string `hcl:"drafts_index_name,optional"`
+	InternalIndexName      string `hcl:"internal_index_name,optional"`
+	LinksIndexName         string `hcl:"links_index_name,optional"`
+	MissingFieldsIndexName string `hcl:"missing_fields_index_name,optional"`
+	ProjectsIndexName      string `hcl:"projects_index_name,optional"`
 }
 
 // NewAdapter creates a new Algolia search adapter.
@@ -34,10 +40,12 @@ func NewAdapter(cfg *Config) (*Adapter, error) {
 	client := search.NewClient(cfg.AppID, cfg.WriteAPIKey)
 
 	return &Adapter{
-		client:      client,
-		docsIndex:   client.InitIndex(cfg.DocsIndexName),
-		draftsIndex: client.InitIndex(cfg.DraftsIndexName),
-		appID:       cfg.AppID,
+		client:        client,
+		docsIndex:     client.InitIndex(cfg.DocsIndexName),
+		draftsIndex:   client.InitIndex(cfg.DraftsIndexName),
+		projectsIndex: client.InitIndex(cfg.ProjectsIndexName),
+		linksIndex:    client.InitIndex(cfg.LinksIndexName),
+		appID:         cfg.AppID,
 	}, nil
 }
 
@@ -52,6 +60,20 @@ func (a *Adapter) DocumentIndex() hermessearch.DocumentIndex {
 func (a *Adapter) DraftIndex() hermessearch.DraftIndex {
 	return &draftIndex{
 		index: a.draftsIndex,
+	}
+}
+
+// ProjectIndex returns the project search interface.
+func (a *Adapter) ProjectIndex() hermessearch.ProjectIndex {
+	return &projectIndex{
+		index: a.projectsIndex,
+	}
+}
+
+// LinksIndex returns the links/redirect search interface.
+func (a *Adapter) LinksIndex() hermessearch.LinksIndex {
+	return &linksIndex{
+		index: a.linksIndex,
 	}
 }
 
@@ -143,6 +165,19 @@ func (di *documentIndex) Search(ctx context.Context, query *hermessearch.SearchQ
 	return nil, fmt.Errorf("Search not yet implemented")
 }
 
+func (di *documentIndex) GetObject(ctx context.Context, docID string) (*hermessearch.Document, error) {
+	var doc hermessearch.Document
+	err := di.index.GetObject(docID, &doc)
+	if err != nil {
+		return nil, &hermessearch.Error{
+			Op:  "GetObject",
+			Err: hermessearch.ErrNotFound,
+			Msg: err.Error(),
+		}
+	}
+	return &doc, nil
+}
+
 func (di *documentIndex) GetFacets(ctx context.Context, facetNames []string) (*hermessearch.Facets, error) {
 	// TODO: Implement facet retrieval
 	return nil, fmt.Errorf("GetFacets not yet implemented")
@@ -223,6 +258,19 @@ func (dri *draftIndex) Search(ctx context.Context, query *hermessearch.SearchQue
 	return nil, fmt.Errorf("Search not yet implemented")
 }
 
+func (dri *draftIndex) GetObject(ctx context.Context, docID string) (*hermessearch.Document, error) {
+	var doc hermessearch.Document
+	err := dri.index.GetObject(docID, &doc)
+	if err != nil {
+		return nil, &hermessearch.Error{
+			Op:  "GetObject",
+			Err: hermessearch.ErrNotFound,
+			Msg: err.Error(),
+		}
+	}
+	return &doc, nil
+}
+
 func (dri *draftIndex) GetFacets(ctx context.Context, facetNames []string) (*hermessearch.Facets, error) {
 	// TODO: Implement facet retrieval
 	return nil, fmt.Errorf("GetFacets not yet implemented")
@@ -230,6 +278,119 @@ func (dri *draftIndex) GetFacets(ctx context.Context, facetNames []string) (*her
 
 func (dri *draftIndex) Clear(ctx context.Context) error {
 	_, err := dri.index.ClearObjects()
+	if err != nil {
+		return &hermessearch.Error{
+			Op:  "Clear",
+			Err: hermessearch.ErrIndexingFailed,
+			Msg: err.Error(),
+		}
+	}
+	return nil
+}
+
+// projectIndex implements search.ProjectIndex.
+type projectIndex struct {
+	index *search.Index
+}
+
+func (pi *projectIndex) Index(ctx context.Context, project map[string]any) error {
+	_, err := pi.index.SaveObject(project)
+	if err != nil {
+		return &hermessearch.Error{
+			Op:  "Index",
+			Err: hermessearch.ErrIndexingFailed,
+			Msg: err.Error(),
+		}
+	}
+	return nil
+}
+
+func (pi *projectIndex) Delete(ctx context.Context, projectID string) error {
+	_, err := pi.index.DeleteObject(projectID)
+	if err != nil {
+		return &hermessearch.Error{
+			Op:  "Delete",
+			Err: hermessearch.ErrNotFound,
+			Msg: err.Error(),
+		}
+	}
+	return nil
+}
+
+func (pi *projectIndex) Search(ctx context.Context, query *hermessearch.SearchQuery) (*hermessearch.SearchResult, error) {
+	// TODO: Implement full search with query parameters
+	return nil, fmt.Errorf("Search not yet implemented")
+}
+
+func (pi *projectIndex) GetObject(ctx context.Context, projectID string) (map[string]any, error) {
+	var project map[string]any
+	err := pi.index.GetObject(projectID, &project)
+	if err != nil {
+		return nil, &hermessearch.Error{
+			Op:  "GetObject",
+			Err: hermessearch.ErrNotFound,
+			Msg: err.Error(),
+		}
+	}
+	return project, nil
+}
+
+func (pi *projectIndex) Clear(ctx context.Context) error {
+	_, err := pi.index.ClearObjects()
+	if err != nil {
+		return &hermessearch.Error{
+			Op:  "Clear",
+			Err: hermessearch.ErrIndexingFailed,
+			Msg: err.Error(),
+		}
+	}
+	return nil
+}
+
+// linksIndex implements search.LinksIndex.
+type linksIndex struct {
+	index *search.Index
+}
+
+func (li *linksIndex) SaveLink(ctx context.Context, link map[string]string) error {
+	_, err := li.index.SaveObject(link)
+	if err != nil {
+		return &hermessearch.Error{
+			Op:  "SaveLink",
+			Err: hermessearch.ErrIndexingFailed,
+			Msg: err.Error(),
+		}
+	}
+	return nil
+}
+
+func (li *linksIndex) DeleteLink(ctx context.Context, objectID string) error {
+	_, err := li.index.DeleteObject(objectID)
+	if err != nil {
+		return &hermessearch.Error{
+			Op:  "DeleteLink",
+			Err: hermessearch.ErrNotFound,
+			Msg: err.Error(),
+		}
+	}
+	return nil
+}
+
+func (li *linksIndex) GetLink(ctx context.Context, objectID string) (map[string]string, error) {
+	var link map[string]string
+	err := li.index.GetObject(objectID, &link)
+	if err != nil {
+		return nil, &hermessearch.Error{
+			Op:  "GetLink",
+			Err: hermessearch.ErrNotFound,
+			Msg: err.Error(),
+		}
+	}
+	return link, nil
+}
+
+func (li *linksIndex) Clear(ctx context.Context) error {
+	_, err := li.index.ClearObjects()
 	if err != nil {
 		return &hermessearch.Error{
 			Op:  "Clear",
